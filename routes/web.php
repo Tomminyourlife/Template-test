36d8abbe7e2e4661be33a66325e1609d99fe0c9f
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-01-31 12:02:15 +0100

    aggiornate rotte

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DataController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,25 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/inserimento-dati', [DataController::class, 'mostraForm']);
+Route::post('/inserimento-dati', [DataController::class, 'inserisciDati']);
+
+Route::get('/modifica-record/{id}', [DataController::class, 'mostraModifica'])->name('mostra-modifica');;
+Route::put('/salva-modifica/{id}', [DataController::class, 'salvaModifica'])->name('salva-modifica');
+
+Route::get('/visualizza-dati', [DataController::class, 'visualizzaDati'])->name('visualizza-dati');
+
+Route::get('/elimina-record/{id}', [DataController::class, 'eliminaRecord'])->name('elimina-record');
+
+
+use App\Http\Controllers\UserController;
+
+Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
+Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');
+Route::post('/admin/settings/update/{id}', [UserController::class, 'updateUser'])->name('admin.settings.update');
+
+
 
 
 

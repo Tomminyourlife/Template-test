407dfb7193448c5452c64abae8f438baf3c322b1
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-01-31 12:11:45 +0100

    rimossi file per manovrare dati in tabella

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\DataController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 
 /*
@@ -23,19 +23,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/inserimento-dati', [DataController::class, 'mostraForm']);
-Route::post('/inserimento-dati', [DataController::class, 'inserisciDati']);
-
-Route::get('/modifica-record/{id}', [DataController::class, 'mostraModifica'])->name('mostra-modifica');;
-Route::put('/salva-modifica/{id}', [DataController::class, 'salvaModifica'])->name('salva-modifica');
-
-Route::get('/visualizza-dati', [DataController::class, 'visualizzaDati'])->name('visualizza-dati');
-
-Route::get('/elimina-record/{id}', [DataController::class, 'eliminaRecord'])->name('elimina-record');
-
-
-use App\Http\Controllers\UserController;
 
 Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
 Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');

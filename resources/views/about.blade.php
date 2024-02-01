8f5bf18e17ef89d2ebc4b6ad4ed793e6ce76b5fd
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-01 17:52:09 +0100

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,4 +8,14 @@
         <p>Find us on GitHub: <a href=https://github.com/Tomminyourlife/Template-test/tree/master> GitHub Repository</a></p>
         <!-- Aggiungi il contenuto della tua pagina -->
     </div>
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-danger"><i class="far fa-star"></i>
+            </span>
+            <div class="info-box-content">
+                <span class="info-box-text">Likes</span>
+                <span class="info-box-number">93,139</span>
+            </div>
+        </div>
+    </div>
 @endsection

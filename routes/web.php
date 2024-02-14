111b6c72733c4278e2d256d1a6c68ab4b1484625
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-14 12:24:38 +0100

    modifiche rotte

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ChatbotController;
+use App\Http\Controllers\WelcomeController;
 
 
 /*
@@ -16,22 +17,23 @@ use App\Http\Controllers\ChatbotController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
+    Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');
+    Route::post('/admin/settings/update/{id}', [UserController::class, 'updateUser'])->name('admin.settings.update');
 
-Route::get('/', function () {
-    return view('welcome');
+
+    Route::get('/admin/pages/about', [ChatbotController::class, 'showChat'])->name('chat.show');
+    Route::post('/admin/pages/about', [ChatbotController::class, 'handleChat'])->name('chat.handle');
 });
+
+Route::get('/', [WelcomeController::class, 'index']);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
-Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');
-Route::post('/admin/settings/update/{id}', [UserController::class, 'updateUser'])->name('admin.settings.update');
 
-
-Route::get('/admin/pages/about', [ChatbotController::class, 'showChat'])->name('chat.show');
-Route::post('/admin/pages/about', [ChatbotController::class, 'handleChat'])->name('chat.handle');
 
 
 

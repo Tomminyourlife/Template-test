e8b1a55643363ce4441d28c5f4ec9893d7adaa6d
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-15 16:00:00 +0100

    Update WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    /**
+    /*
      * Display the welcome page.
      *
      * @return \Illuminate\Contracts\Support\Renderable

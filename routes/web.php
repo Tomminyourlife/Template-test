9b7edcbb442163bd149bac891e2c9fe449be5227
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-15 16:00:03 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,6 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\ChatController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::middleware(['web', 'auth'])->group(function () {
     Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
     Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');

bb579332b20719d962f2185e525f9ea5a17656c9
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-15 17:38:00 +0100

    Modifica scritte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Benvenuto</div>
+                    <div class="card-header"><h3>Benvenuto</h3></div>
 
                     <div class="card-body">
                         <div id="chat-container">
-                            <div id="chat-header">ChatBot</div>
+                            <div id="chat-header"><h5>Ciao!</h5></div>
                             <div id="chat-messages">
                                 <!-- Loop per mostrare i messaggi della chat -->
                                 <div v-for="message in chatHistory" :class="message.sender + '-message'">@{{ message.text }}</div>

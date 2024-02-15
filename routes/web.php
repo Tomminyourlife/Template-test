397568e6908d28f0f5d88cf742d0c7c9900b056e
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-15 15:08:17 +0100

    agg. rotte

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ChatbotController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ChatController;
 
 
 /*
@@ -21,13 +21,10 @@ Route::middleware(['web', 'auth'])->group(function () {
     Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
     Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');
     Route::post('/admin/settings/update/{id}', [UserController::class, 'updateUser'])->name('admin.settings.update');
-
-
-    Route::get('/admin/pages/about', [ChatbotController::class, 'showChat'])->name('chat.show');
-    Route::post('/admin/pages/about', [ChatbotController::class, 'handleChat'])->name('chat.handle');
 });
 
 Route::get('/', [WelcomeController::class, 'index']);
+Route::post('/', [ChatController::class, 'getBotResponse']);
 
 Auth::routes();
 

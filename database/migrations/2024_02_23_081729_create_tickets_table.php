b910efb116620dbf62ce6e92a578e2f84f9da495
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-23 11:33:22 +0100

    create nuove tabelle

---
+++ database/migrations/2024_02_23_081729_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); // Chiave esterna per l'utente associato al ticket
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('category_id')->nullable()->constrained('categories'); // Chiave esterna per la categoria del ticket
+            $table->foreignId('attachment_id')->nullable()->constrained('attachments'); // Chiave esterna per l'allegato del ticket
+            $table->enum('status', ['aperto', 'in_corso', 'chiuso'])->default('aperto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
---
+++ database/migrations/2024_02_23_100454_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nome della categoria (ad es. 'Assistenza Tecnica')
+            $table->unsignedBigInteger('team_id'); // Chiave esterna per il team di riferimento
+            $table->timestamps();
+
+            // Chiave esterna per il team di riferimento
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_categories');
+    }
+}
+

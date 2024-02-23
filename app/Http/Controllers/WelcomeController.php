d954092383a499ff5497ac2371fffd4cf0bb5550
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-23 11:28:31 +0100

    modifiche per chatbot

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
-class WelcomeController extends Controller
-{
+class WelcomeController extends Controller{
     /*
      * Display the welcome page.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('welcome');
+    public function index(){
+        $giacomo = Customer::find(5519);
+        return view('welcome', compact('giacomo'));
+    }
+
+    public function checkVat(Request $request){
+        $inputVat = $request->input('vat_number');
+
+        // Esegui il controllo nel database
+        $user = Customer::where('pi', $inputVat)->first();
+
+        if ($user) {
+            $message = "Ciao $user->nome! Di cosa ha bisogno?";
+            return response()->json(['message' => $message]);
+        } else {
+            return response()->json(['message' => 'Siamo spiacenti, la partita IVA non è corretta o non fai parte dei nostri clienti']);
+        }
     }
 }

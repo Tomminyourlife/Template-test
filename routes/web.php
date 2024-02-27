020b023f69dd2ffa40e09c97d59ed365baf6d9b1
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-27 17:14:36 +0100

    sistemate rotte category/ticket

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\ChatController;
+use App\Http\Controllers\TicketController;
 
 
 Route::middleware(['web', 'auth'])->group(function () {
@@ -15,6 +15,8 @@ Route::middleware(['web', 'auth'])->group(function () {
 
 Route::get('/', [WelcomeController::class, 'index']);
 Route::post('/check-vat', [WelcomeController::class, 'checkVat']);
+Route::post('/save-category', [WelcomeController::class, 'saveCategory']);
+Route::post('/create-ticket', [TicketController::class, 'createTicket']);
 
 Auth::routes();
 

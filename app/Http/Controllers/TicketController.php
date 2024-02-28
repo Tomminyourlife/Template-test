12423eaf0659231b0c758a3c401582d31f8cad7b
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-28 17:33:17 +0100

    prove mod.

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,18 +3,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    public function createTicket(Request $request)
-    {
+    public function createTicket(Request $request){
         // Esegui la creazione del ticket con la categoria ricevuta dal client
         // Ad esempio, puoi salvarlo nel database con Eloquent
         $selectedCategory = $request->input('category');
 
-        // Qui dovresti implementare la logica per la creazione del ticket nel tuo sistema
+        // Trova la categoria selezionata
+        //$category = Category::findOrFail($selectedCategory);
+
+        // Crea un nuovo ticket utilizzando il modello Eloquent
+        /*$ticket =Ticket::create([
+            'category' => $selectedCategory,
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            // Aggiungi altri campi necessari
+        ]);
+
+        return redirect()->route('ticket.show', ['id' => $ticket->id])
+            ->with('success', 'Ticket creato con successo');*/
 
         return response()->json(['message' => 'Ticket creato con successo']);
     }
+
+    /*public function show($id){
+        // Recupera il ticket dal database utilizzando il modello Eloquent
+        $ticket = Ticket::findOrFail($id);
+
+        $team = $ticket->category->team;
+
+        // Passa il ticket alla vista e visualizza la pagina del ticket
+        return view('ticket.show', ['ticket' => $ticket]);
+    }*/
 }

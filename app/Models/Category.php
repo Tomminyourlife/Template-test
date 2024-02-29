12fbdf317faef9e91df356675746ee7d9f515346
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-02-29 16:04:26 +0100

    agg. model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
+class Category extends Model{
+
+    protected $table = 'categories';
+    protected $primaryKey = 'id'; 
+    public $timestamps = true;
+    protected $fillable = [
+        'name',
+        'description',
+    ]; 
+
     // Definisci la relazione tra la categoria e i team
     public function team(){
         return $this->belongsTo(Team::class);

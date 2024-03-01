dfb76baf2f75298ecee73ec1d65329e854a16604
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-01 10:10:56 +0100

    model allegati

---
+++ app/Models/Attachment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    protected $fillable = [
+        'ticket_id',
+        'filename',
+        // Aggiungi altri campi per la gestione degli allegati, se necessario
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+}
+

48ea8492f609ad66dea0ff50c1919fab9f0b0def
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-05 17:04:29 +0100

    mod.

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -10,16 +10,32 @@ class Ticket extends Model
     protected $primaryKey = 'id'; 
     public $timestamps = true;
     protected $fillable = [
+        'customer_id',
         'category_id',
         'description',
         //file?
     ]; 
+
+    /*protected $casts = [
+        'attachments' => 'array',
+    ];
+
+    public function addAttachment($path){
+        $attachments = $this->attachments ?? [];
+        $attachments[] = $path;
+        $this->attachments = $attachments;
+        $this->save();
+    }*/
 
     public function category(){
         return $this->belongsTo(Category::class);
     }
 
     public function attachments(){
-        return $this->hasMany(Attachment::class);
+        return $this->hasMany(Attachment::class, 'attachment_id');
+    }
+
+    public function customer(){
+        return $this->belongsTo(Customer::class, 'customer_id');
     }
 }

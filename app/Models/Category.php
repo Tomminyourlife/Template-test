33773491df60718985cfd27f6f98584e830016f7
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-06 17:53:02 +0100

    mod.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -20,6 +20,6 @@ class Category extends Model{
     }
 
     public function tickets(){
-        return $this->hasMany(Ticket::class);
+        return $this->hasMany(Ticket::class, 'category_id');
     }
 }

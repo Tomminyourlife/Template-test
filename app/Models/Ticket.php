483478b49e4e425cff778ee9e32a6c428ca0643e
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-11 12:34:11 +0100

    mod.

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -13,6 +13,7 @@ class Ticket extends Model
         'customer_id',
         'category_id',
         'description',
+        'title',
     ]; 
     
     public function category(){
@@ -30,4 +31,23 @@ class Ticket extends Model
     public function team(){
     return $this->belongsTo(Team::class);
     }
+    
+// GESTIONE STATI TICKET
+    /*const STATUS_OPEN = 'aperto';
+    const STATUS_CLOSED = 'chiuso';
+    const STATUS_WORKING = 'in_corso';
+
+    public static function getStatuses(){
+        return [
+            self::STATUS_OPEN,
+            self::STATUS_CLOSED,
+            self::STATUS_WORKING,
+            // Aggiungi altri stati se necessario
+        ];
+    }
+
+    public function close(){
+        $this->status = self::STATUS_CLOSED;
+        $this->save();
+    }*/
 }

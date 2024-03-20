feecbf212edadce16a8f0d97ce789f4a24154978
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-20 17:45:21 +0100

    aggiunte rotte per ticket backend

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -10,6 +11,10 @@ Route::middleware(['web', 'auth'])->group(function () {
     Route::get('/admin/settings', [UserController::class, 'showSettings'])->name('admin_settings');
     Route::get('/admin/settings/edit/{id}', [UserController::class, 'editUser'])->name('admin.settings.edit');
     Route::post('/admin/settings/update/{id}', [UserController::class, 'updateUser'])->name('admin.settings.update');
+    Route::get('/admin/pages/alltickets', [TicketController::class, 'index'])->name('tickets.index');
+    Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
+    Route::post('/tickets/{ticketId}/addComment', [TicketController::class, 'addComment'])->name('tickets.addComment');
+    Route::put('/tickets/{id}', [TicketController::class, 'update'])->name('tickets.update');
 });
 
 Route::get('/', [WelcomeController::class, 'index']);

6c567e1b9039c0463fcfd1476d06def8faddc685
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-21 12:41:38 +0100

    modificato reindirizzamento dopo il login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/pages/alltickets';
 
     /**
      * Create a new controller instance.

54bbf90076c3a4d79bd1d10e868cf262fecfb79a
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-25 12:39:46 +0100

    dashboard.blade del cliente

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin Dashboard')
+
+@section('content_header')
+    <h1>Welcome to Admin Dashboard</h1>
+@endsection
+
+@section('content')
+    <!-- Contenuto della dashboard -->
+@endsection
+

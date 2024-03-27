c2fa544495a2fd01b945600e9bb5d615d2b380c9
Tomminyourlife <tommaso.bianchi@inyourlife.info>
2024-03-27 12:25:20 +0100

    modifica scritta chatbot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header"><h2><b>Benvenuto</b></h2></div>
+                    <div class="card-header"><h2><b>Benvenuto! Crea qua il tuo ticket</b></h2></div>
 
                     <div class="card-body">
                         <div id="chat-container">
